00a369027c157dacc0b8e102904009083862e0f6
codefyp <idivertor@gmail.com>
2022-04-10 09:09:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+headsortales = random.randint(1, 2)
+
+if headsortales == 1 :
+ print("I'ts Heads")
+else :
+ print("I'ts Tails")
+
+
+
+
+
+
